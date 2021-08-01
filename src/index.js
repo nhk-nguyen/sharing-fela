adfb01879903a309ddb8b37ba6e2e8f6f545bb26
Kris <macus@KrisNguyen.local>
2021-08-01 22:58:10 +0700

    first-end

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import './scss/main.scss'
+import './scss/main.scss';
+import { createRenderer } from 'fela';
+import { render } from 'fela-dom';
+
 
 const root = document.getElementById('root');
+
+const rule = () =>  ({
+  backgroundColor: 'black',
+  color: 'white',
+  width: '100%',
+  height: '50vh',
+  fontSize: '40px',
+  textAlign: 'center',
+  lineHeight: '50vh'
+})
+
+const renderer = createRenderer();
+
+render(renderer);
+
+const className = renderer.renderRule(rule);
+console.log(className, 'className');
+
+root.classList = className
